65a0702a8abfc50d93c0b56da6768fa1f570a763
blandine <bdescamps@viareport.com>
2013-10-10 10:34:40 +0200

    intégration de inativ-gruntfile

--- test/test.js
+++ test/test.js
@@ -1,13 +1,17 @@
-var TestSuite = require('spatester').TestSuite;
+var TestSuite = require('spatester');
 
 var testSuite = new TestSuite("WEB COMPONENT test", {});
 
-Testem.useCustomAdapter(function(socket) {
-    testSuite.setSocket(socket);
-});
+//Testem.useCustomAdapter(function(socket) {
+//    testSuite.setSocket(socket);
+//});
 
 /** TODO : écrire les tests **/
-
+testSuite.addTest("le test qui sert à rien", function (scenario, asserter) {
+    asserter.assertTrue(function(){
+        return true;
+    });
+});
 
 document.addEventListener('DOMComponentsLoaded', function(){
     testSuite.run();
